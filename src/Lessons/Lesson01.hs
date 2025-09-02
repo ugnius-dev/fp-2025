0bce43b4b6762178877d6beb0995d90c1770ca28
ViPo <pi@homedir.eu>
2025-09-02 14:34:06 +0000

    Lesson01

--- src/Lessons/Lesson01.hs
+++ src/Lessons/Lesson01.hs
@@ -1,1 +1,31 @@
-module Lessons.Lesson01 () where
+-- | Module is a unit of compilation.
+
+module Lessons.Lesson01 (i, ii, c, s, b, f, add, il, cl) where
+
+
+i :: Integer
+i = 42
+
+ii :: Int
+ii = 43
+
+c :: Char
+c = 'a'
+
+s :: String
+s = "labas"
+
+b :: Bool
+b = True
+
+f :: Integer -> Bool
+f age = age >= 20
+
+add :: Integer -> Integer -> Integer
+add a b = a + b
+
+il :: [Integer]
+il = [42, 42, 42]
+
+cl :: [Char]
+cl = "labas"
