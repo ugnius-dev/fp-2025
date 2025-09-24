57a46b1f37fd82f46b7294e633c7585fe9b8dea3
Ugnius Vaičeskas <70817570+ugnius-dev@users.noreply.github.com>
2025-09-24 04:36:14 +0300

    Pirmos uzduoties Haskell kodas

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,12 +5,70 @@ module Lib1
 data Dumpable = Examples
   deriving Show
 
+data PrepositionTo = To deriving Show
+
+data ChoiceOption = ChoiceOption
+  { text :: String
+  , next :: Story
+  }
+  deriving Show
+
+data ChapterElement =
+  Title String
+  | Paragraph String
+  | Dialogue String String
+  | Monologue String
+  | Picture {url :: String, altText :: String}
+  | Choice [ChoiceOption]
+  deriving Show
+
+data Story =
+  Story [ChapterElement]
+  | Add ChapterElement PrepositionTo Story
+  | MergeStories Story Story
+  | EmptyStory
+  deriving Show
+
 -- This is a "root" ADT representing your grammar,
 -- Please expand this ADT as needed
 data Command = Dump Dumpable
+  | Create Story
+  | Publish Story String PrepositionTo String
   deriving Show
 
 examples :: [Command]
 examples = [
-    Dump Examples
-    ]
+    Dump Examples,
+    Create (Story
+      [
+        Title "Intro",
+        Paragraph "You wake up in a small village, the sun rising over the hills."
+      ]
+    ),
+    Create (Story
+      [ Dialogue "Villager" "Welcome to our village!",
+        Monologue "I hope today will be an adventure.",
+        Picture { url = "http://example.com/village.png", altText = "Village view" }
+      ]
+    ),
+    Publish (Story [Title "Intro"]) "Amazing Book" To "My Blog",
+    Create (Story
+      [
+        Paragraph "You come to a fork in the road.",
+        Choice [
+          ChoiceOption "Take the left path" (Story
+            [ Paragraph "The forest is dark and quiet.",
+              Choice [
+                ChoiceOption "Go back to crossroads" (Story
+                  [ Paragraph "You are back at the crossroads." ]
+                )
+              ]
+            ]
+          ),
+          ChoiceOption "Take the right path" (Story
+            [ Paragraph "A small village appears in the distance." ]
+          )
+        ]
+      ]
+    )
+  ]
